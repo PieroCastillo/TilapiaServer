81e76da859230e9f9907b3304e2a853e2958a702
Piero Castillo <pierocastillo2005@gmail.com>
2026-04-25 00:35:35 -0500

    changes in IR

--- include/Tilapia.Runtime/ir.hpp
+++ include/Tilapia.Runtime/ir.hpp
@@ -18,196 +18,193 @@ namespace TilapiaServer::Runtime::IR
     */
     enum class coreCaps : uint16_t
     {
-        control      = 0x0000,
-        alu          = 0x0001,
-        time         = 0x0002,
-        random       = 0x0003,
-        print        = 0x0004,
-        memory       = 0x0005,
-        atomic       = 0x0006,
-        sync         = 0x0007,
-        simd         = 0x0008,
-        tls13        = 0x0010,
-        quic         = 0x0011,
-        http3        = 0x0012,
+        control = 0x0000,
+        alu = 0x0001,
+        memory = 0x0002,
+        time = 0x0003,
+        simd = 0x0004,
+        random = 0x0005,
+        print = 0x0006,
+        tls13 = 0x0010,
+        quic = 0x0011,
+        http3 = 0x0012,
         webtransport = 0x0013,
     };
 
     enum class coreDatatype : uint16_t
     {
-        u8        = 0x0001,
-        u16       = 0x0002,
-        u32       = 0x0004,
-        u64       = 0x0008,
-        u128      = 0x0010,
-        i8        = 0x0011,
-        i16       = 0x0012,
-        i32       = 0x0014,
-        i64       = 0x0018,
-        f32       = 0x0024,
-        f64       = 0x0028,
-        boolean   = 0x0031,
+        u8 = 0x0001,
+        u16 = 0x0002,
+        u32 = 0x0004,
+        u64 = 0x0008,
+        u128 = 0x0010,
+        i8 = 0x0011,
+        i16 = 0x0012,
+        i32 = 0x0014,
+        i64 = 0x0018,
+        f32 = 0x0024,
+        f64 = 0x0028,
+        boolean = 0x0031,
 
         memhandle = 0x0100,
         reshandle = 0x0200,
-        string    = 0x0300,
-        span      = 0x0400,
+        string = 0x0300,
+        span = 0x0400,
     };
 
-    enum class coreBaseOpcodes : uint32_t
-    {   
-        // CONTROL : 0x00
-        nop          = 0x00000000,
-        push_var     = 0x00000010,
-        push_imm_i32 = 0x00000011,
-        push_const   = 0x00000012,
-        pop_var      = 0x00000015,
-        call_local   = 0x00000020,
-        call_cap     = 0x00000021,
-        call_ext     = 0x00000022,
-
-        // ALU INT : 0x0100
-        add_i        = 0x01000001,
-        sub_i        = 0x01000002,
-        mul_i        = 0x01000003,
-        div_i        = 0x01000004,
-        mod_i        = 0x01000005,
-
-        // ALU FLOAT : 0x0101
-        add_f        = 0x01010001,
-        sub_f        = 0x01010002,
-        mul_f        = 0x01010003,
-        div_f        = 0x01010004,
-
-        // ALU BITWISE/LOGIC : 0x0102
-        and_b        = 0x01020001,
-        or_b         = 0x01020002,
-        xor_b        = 0x01020003,
-        not_b        = 0x01020004,
-        shl_b        = 0x01020005,
-        shr_b        = 0x01020006,
-
-        // ALU COMP INT : 0x0103
-        cmp_eq_i     = 0x01030001,
-        cmp_ne_i     = 0x01030002,
-        cmp_lt_i     = 0x01030003,
-        cmp_gt_i     = 0x01030004,
-
-        // ALU COMP FLOAT : 0x0104
-        cmp_eq_f     = 0x01040001,
-        cmp_lt_f     = 0x01040002,
-        cmp_gt_f     = 0x01040003,
-
-        // MEMORY : 0x05
-        load_u8      = 0x05000001,
-        load_i32     = 0x05000002,
-        load_i64     = 0x05000003,
-        load_f64     = 0x05000004,
-
-        store_u8     = 0x05010001,
-        store_i32    = 0x05010002,
-        store_i64    = 0x05010003,
-        store_f64    = 0x05010004,
-
-        memcpy       = 0x05020001,
-        memmove      = 0x05020002,
-        memset       = 0x05020003,
-
-        alloc        = 0x05030001,
-        free         = 0x05030002,
-        protect      = 0x05030003,
-    };
-
-    enum class coreExtCalls : uint32_t
+    enum class coreOpcodes : uint32_t
     {
-        // TIME : 0x02
-        now_monotonic = 0x02000001,
-        now_wallclock = 0x02000002,    
-        
-        // SIMD INT : 0x0800
-        vadd_i        = 0x08000001,
-        vsub_i        = 0x08000002,
-        vmul_i        = 0x08000003,
-        vdiv_i        = 0x08000004,
+        // CONTROL : 0x0000
+        nop = 0x00000000,
+        push_var = 0x00000001,
+        push_imm_i32 = 0x00000002,
+        push_const = 0x00000003,
+        pop_var = 0x00000004,
+        call_local = 0x00000005,
+        call_cap = 0x00000006,
+        call_ext = 0x00000007,
 
-        // SIMD FLOAT : 0x0801
-        vadd_f        = 0x08010001,
-        vsub_f        = 0x08010002,
-        vmul_f        = 0x08010003,
-        vdiv_f        = 0x08010004,
+        // ALU INT : 0x0001
+        add_i = 0x00010001,
+        sub_i = 0x00010002,
+        mul_i = 0x00010003,
+        div_i = 0x00010004,
+        mod_i = 0x00010005,
 
-        // SIMD BITWISE : 0x0802
-        vand_b        = 0x08020001,
-        vor_b         = 0x08020002,
-        vxor_b        = 0x08020003,
+        // ALU FLOAT : 0x0001
+        add_f = 0x00010006,
+        sub_f = 0x00010007,
+        mul_f = 0x00010008,
+        div_f = 0x00010009,
 
-        // SIMD MEMORY/UTILS : 0x0803
-        vload         = 0x08030001,
-        vstore        = 0x08030002,
-        vshuffle      = 0x08030003,
-        vreduce_add_i = 0x08030004,
-        vreduce_add_f = 0x08030005,
+        // ALU BITWISE/LOGIC : 0x0001
+        and_b = 0x0001000A,
+        or_b = 0x0001000B,
+        xor_b = 0x0001000C,
+        not_b = 0x0001000D,
+        shl_b = 0x0001000E,
+        shr_b = 0x0001000F,
+
+        // ALU COMP INT : 0x0001
+        cmp_eq_i = 0x00010010,
+        cmp_ne_i = 0x00010011,
+        cmp_lt_i = 0x00010012,
+        cmp_gt_i = 0x00010013,
+
+        // ALU COMP FLOAT : 0x0001
+        cmp_eq_f = 0x00010014,
+        cmp_lt_f = 0x00010015,
+        cmp_gt_f = 0x00010016,
+
+        // MEMORY : 0x0002
+        load_u8 = 0x00020001,
+        load_i32 = 0x00020002,
+        load_i64 = 0x00020003,
+        load_f64 = 0x00020004,
+
+        store_u8 = 0x00020005,
+        store_i32 = 0x00020006,
+        store_i64 = 0x00020007,
+        store_f64 = 0x00020008,
+
+        memcpy = 0x00020009,
+        memmove = 0x0002000A,
+        memset = 0x0002000B,
+
+        alloc = 0x0002000C,
+        free = 0x0002000D,
+        protect = 0x0002000E,
+
+        // EXTENSION OPERATIONS
+
+        // TIME : 0x0003
+        now_monotonic = 0x00030001,
+        now_wallclock = 0x00030002,
+
+        // SIMD INT : 0x0004
+        vadd_i = 0x00040001,
+        vsub_i = 0x00040002,
+        vmul_i = 0x00040003,
+        vdiv_i = 0x00040004,
+
+        // SIMD FLOAT : 0x0004
+        vadd_f = 0x00040005,
+        vsub_f = 0x00040006,
+        vmul_f = 0x00040007,
+        vdiv_f = 0x00040008,
+
+        // SIMD BITWISE : 0x0004
+        vand_b = 0x00040009,
+        vor_b = 0x0004000A,
+        vxor_b = 0x0004000B,
+
+        // SIMD MEMORY/UTILS : 0x0004
+        vload = 0x0004000C,
+        vstore = 0x0004000D,
+        vshuffle = 0x0004000E,
+        vreduce_add_i = 0x0004000F,
+        vreduce_add_f = 0x00040010,
     };
 
     enum class coreCalls : uint32_t
     {
-        // RANDOM : 0x03
-        seed              = 0x03000001,
-        next_u32          = 0x03000002,
-        next_u64          = 0x03000003,
-        next_f32          = 0x03000004,
-        next_f64          = 0x03000005,
-        fill_bytes        = 0x03000006,
-        secure_fill_bytes = 0x03000007,
+        // RANDOM : 0x0005
+        seed = 0x00050001,
+        next_u32 = 0x00050002,
+        next_u64 = 0x00050003,
+        next_f32 = 0x00050004,
+        next_f64 = 0x00050005,
+        fill_bytes = 0x00050006,
+        secure_fill_bytes = 0x00050007,
 
-        // PRINT / DEBUG : 0x04
-        print_str         = 0x04000001,
-        print_i32         = 0x04000002,
-        print_i64         = 0x04000003,
-        print_f32         = 0x04000004,
-        print_f64         = 0x04000005,
-        flush             = 0x04000006,
+        // PRINT / DEBUG : 0x0006
+        print_str = 0x00060001,
+        print_i32 = 0x00060002,
+        print_i64 = 0x00060003,
+        print_f32 = 0x00060004,
+        print_f64 = 0x00060005,
+        flush = 0x00060006,
 
-        // TLS 1.3 : 0x10
-        tls_context_create      = 0x10000001,
-        tls_handshake_start     = 0x10000002,
-        tls_encrypt             = 0x10000003,
-        tls_decrypt             = 0x10000004,
+        // TLS 1.3 : 0x1000
+        tls_context_create = 0x10000001,
+        tls_handshake_start = 0x10000002,
+        tls_encrypt = 0x10000003,
+        tls_decrypt = 0x10000004,
         tls_export_key_material = 0x10000005,
-        tls_close               = 0x10000006,
+        tls_close = 0x10000006,
 
-        // QUIC : 0x11
-        quic_endpoint_create    = 0x11000001,
-        quic_connect            = 0x11000002,
-        quic_accept             = 0x11000003,
-        quic_stream_open        = 0x11000004,
-        quic_stream_accept      = 0x11000005,
-        quic_stream_send        = 0x11000006,
-        quic_stream_recv        = 0x11000007,
-        quic_datagram_send      = 0x11000008,
-        quic_datagram_recv      = 0x11000009,
-        quic_close_stream       = 0x1100000A,
-        quic_close_connection   = 0x1100000B,
+        // QUIC : 0x1100
+        quic_endpoint_create = 0x11000001,
+        quic_connect = 0x11000002,
+        quic_accept = 0x11000003,
+        quic_stream_open = 0x11000004,
+        quic_stream_accept = 0x11000005,
+        quic_stream_send = 0x11000006,
+        quic_stream_recv = 0x11000007,
+        quic_datagram_send = 0x11000008,
+        quic_datagram_recv = 0x11000009,
+        quic_close_stream = 0x1100000A,
+        quic_close_connection = 0x1100000B,
 
-        // HTTP/3 : 0x12
-        http3_session_create    = 0x12000001,
-        http3_request_send      = 0x12000002,
-        http3_response_recv     = 0x12000003,
-        http3_headers_send      = 0x12000004,
-        http3_headers_recv      = 0x12000005,
-        http3_body_send         = 0x12000006,
-        http3_body_recv         = 0x12000007,
-        http3_session_close     = 0x12000008,
+        // HTTP/3 : 0x1200
+        http3_session_create = 0x12000001,
+        http3_request_send = 0x12000002,
+        http3_response_recv = 0x12000003,
+        http3_headers_send = 0x12000004,
+        http3_headers_recv = 0x12000005,
+        http3_body_send = 0x12000006,
+        http3_body_recv = 0x12000007,
+        http3_session_close = 0x12000008,
 
-        // WEBTRANSPORT : 0x13
-        wt_session_create       = 0x13000001,
-        wt_connect              = 0x13000002,
-        wt_accept               = 0x13000003,
-        wt_stream_open          = 0x13000004,
-        wt_stream_accept        = 0x13000005,
-        wt_datagram_send        = 0x13000006,
-        wt_datagram_recv        = 0x13000007,
-        wt_close                = 0x13000008,
+        // WEBTRANSPORT : 0x1300
+        wt_session_create = 0x13000001,
+        wt_connect = 0x13000002,
+        wt_accept = 0x13000003,
+        wt_stream_open = 0x13000004,
+        wt_stream_accept = 0x13000005,
+        wt_datagram_send = 0x13000006,
+        wt_datagram_recv = 0x13000007,
+        wt_close = 0x13000008,
     };
 
     enum class symbolType : uint16_t
--- src/Tilapia.Runtime/app.cpp
+++ src/Tilapia.Runtime/app.cpp
@@ -38,11 +38,88 @@ int main(int argc, char** argv) {
     /*
     execBinary = load(asmPath); // converts raw data to spec-valid IR Assembly
     validate(execBinary); // check caps
-    while()
+    while()0x0400IR
     */
 
     binary executable;
     Instance executionState;
+    ConfigureInstance(&executable, &executionState, 4 * 1024 * 1024); // 4 MB arena
 
+    auto& program = executable.instructions;
+    while (executionState.isRunning)
+    {
+        const IR::instruction& inst = executable.instructions[executionState.ip++];
+        uint32_t op = (inst.capId << 16) & inst.opCode;
+
+        switch (static_cast<IR::coreOpcodes>(op))
+        {
+        case IR::coreOpcodes::nop:
+            break;
+        case IR::coreOpcodes::push_var:
+        case IR::coreOpcodes::push_imm_i32:
+        case IR::coreOpcodes::push_const:
+        case IR::coreOpcodes::pop_var:
+        case IR::coreOpcodes::call_local:
+        case IR::coreOpcodes::call_cap:
+        case IR::coreOpcodes::call_ext:
+        case IR::coreOpcodes::add_i:
+        case IR::coreOpcodes::sub_i:
+        case IR::coreOpcodes::mul_i:
+        case IR::coreOpcodes::div_i:
+        case IR::coreOpcodes::mod_i:
+        case IR::coreOpcodes::add_f:
+        case IR::coreOpcodes::sub_f:
+        case IR::coreOpcodes::mul_f:
+        case IR::coreOpcodes::div_f:
+        case IR::coreOpcodes::and_b:
+        case IR::coreOpcodes::or_b:
+        case IR::coreOpcodes::xor_b:
+        case IR::coreOpcodes::not_b:
+        case IR::coreOpcodes::shl_b:
+        case IR::coreOpcodes::shr_b:
+        case IR::coreOpcodes::cmp_eq_i:
+        case IR::coreOpcodes::cmp_ne_i:
+        case IR::coreOpcodes::cmp_lt_i:
+        case IR::coreOpcodes::cmp_gt_i:
+        case IR::coreOpcodes::cmp_eq_f:
+        case IR::coreOpcodes::cmp_lt_f:
+        case IR::coreOpcodes::cmp_gt_f:
+        case IR::coreOpcodes::load_u8:
+        case IR::coreOpcodes::load_i32:
+        case IR::coreOpcodes::load_i64:
+        case IR::coreOpcodes::load_f64:
+        case IR::coreOpcodes::store_u8:
+        case IR::coreOpcodes::store_i32:
+        case IR::coreOpcodes::store_i64:
+        case IR::coreOpcodes::store_f64:
+        case IR::coreOpcodes::memcpy:
+        case IR::coreOpcodes::memmove:
+        case IR::coreOpcodes::memset:
+        case IR::coreOpcodes::alloc:
+        case IR::coreOpcodes::free:
+        case IR::coreOpcodes::protect:
+            break;
+            // EXTENSION OPS
+        case IR::coreOpcodes::now_monotonic:
+        case IR::coreOpcodes::now_wallclock:
+        case IR::coreOpcodes::vadd_i:
+        case IR::coreOpcodes::vsub_i:
+        case IR::coreOpcodes::vmul_i:
+        case IR::coreOpcodes::vdiv_i:
+        case IR::coreOpcodes::vadd_f:
+        case IR::coreOpcodes::vsub_f:
+        case IR::coreOpcodes::vmul_f:
+        case IR::coreOpcodes::vdiv_f:
+        case IR::coreOpcodes::vand_b:
+        case IR::coreOpcodes::vor_b:
+        case IR::coreOpcodes::vxor_b:
+        case IR::coreOpcodes::vload:
+        case IR::coreOpcodes::vstore:
+        case IR::coreOpcodes::vshuffle:
+        case IR::coreOpcodes::vreduce_add_i:
+        case IR::coreOpcodes::vreduce_add_f:
+            break;
+        }
+    }
     return 0;
 }
